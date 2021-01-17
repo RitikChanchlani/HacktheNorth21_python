c3028a2fdc73dabd2c9739ac4a687b817fa1e6f0
Yang Lu <xinyanglu664@gmail.com>
2021-01-16 23:59:50 -0500

    add server

--- HTT21Database/app.py
+++ HTT21Database/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import main
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Test</h1>"
+
+@app.route('/create/user', methods=['GET'])
+def generate_user(){
+    return jsonify({'id': main.add_user('u')})
+}
